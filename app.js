e551e37aa56c811d99c9bb48e478352fbcab83f6
muhammadAli <muhammadAli@gamil.com>
2025-08-24 22:17:58 +0500

    4dars

---
+++ app.js
@@ -0,0 +1,10 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const from = document.querySelector("form");
+let users = [];
+function reducerUsers(users) {
+    users.forEach((item.User), {
+        const: { id, name, age } = item
+    });
+}
+//# sourceMappingURL=app.js.map
---
+++ app.ts
@@ -0,0 +1,14 @@
+const from = document.querySelector("form") as HTMLFormElement;
+type User = {
+    id: number;
+    name: string;
+    age: string;
+
+};
+ let users: User[] = [];
+
+ function reducerUsers(users: User[]) {
+    users.forEach ((item. User) => {
+        const { id, name, age} = item 
+    })
+ }
